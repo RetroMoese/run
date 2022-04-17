3598b5f5fb3a2c8d23f59d26d3416d2bef718ce8
RetroMoese <52784945+RetroMoese@users.noreply.github.com>
2022-04-18 01:53:58 +0300

    добавлнение комонента врага

---
+++ run1/Assets/Global/Components/EnemyComponent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public struct EnemyComponent
+{
+    public Transform obstacleTransform;
+    public Vector3 pointA;
+    public Vector3 pointB;
+}
+
